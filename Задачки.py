090e95d52330dad62a74d2c7f8e3d9c21727f036
Moonmen <tolikdobrozorov@gmail.com>
2024-11-26 20:35:31 +0500

    обучение ткинтеру

--- Задачки.py
+++ Задачки.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 click = 0
 def fullsc(): # ф
@@ -17,8 +18,8 @@ def ehit(): #  можно создать кнопку выхода и встав
 
 def finish():# функция удержания закрытия окна, чтобы можно было вставить дочернее окошко по типу "вы уверены что хотите выйти?". можно обойтись и без него
     win.destroy()
-win = Tk()
-win.title('Name')
+win = Tk() # создаём переменную вин и присвоим функцию нашей библиотеки
+win.title('Name') # тут имя окошка пишется
 win.geometry("720x480")
 win.protocol('WM_DELETE_WINDOW', finish) # Протокол WM_DELETE_WINDOW в Tkinter вызывается, когда окно верхнего уровня должно уже закрываться. По умолчанию Tk уничтожает окно, для которого оно было получено. cпс яндекс нейро
 win.resizable() # даёт свободно растягивать/стягивать окно
@@ -76,7 +77,7 @@ knopka.place(x=10,y=84)
 vivod = ttk.Label() # создаем просто текстовое поле, а даём туда инфу в функции "Внести" выше.
 vivod.place(x=10, y=110)
 knopka_sbrosa = ttk.Button(text='Сбросить', command=sbros) # та самая кнопошка сброса
-knopka_sbrosa.place(x=10,y=130)
+knopka_sbrosa.place(x=10,y=125)
 
 #добавим простейшую аутентификацию
 #создадим переменную пароль
@@ -96,6 +97,35 @@ passchek = ttk.Entry()#создаем поле ввода
 passchek.place(x=10,y=175)
 buttomapss = ttk.Button(text='Ввести пароль', command=chekpass) #создаем кнопку проверки пароля
 buttomapss.place(x=10, y=200)
+
+#Добавим галочку
+def Galka(): # функция галочки
+    if enabled.get() == 1: # условия проверки нажатия на галочку ( true или false; 1 или 0).
+        showinfo(title="Info", message="Второе окно создано.") # из подллибы showinfo
+        ## докидаем сюда ещё функции для галки.
+        tk = Tk() # определяем переменную дял окна
+        tk.title('Окно 2') # Даем название
+        tk.geometry("360x480")# Даем размер
+        info = Label(tk,text='Окно два')# вывод текста во втором окне
+        info.place(x=10, y= 10)# местоположение текста
+        tk.mainloop()# луп окна
+    else: # если enabled.get() = 0
+        showinfo(title="Info", message="Зачем?") # Удаляем новое окошко
+
+
+
+enabled = IntVar() # создание виджета с галочкой
+
+enabled_checkbutton = ttk.Checkbutton(text="Галка", variable=enabled, command=Galka) # variable - значение галки 1 или 0.
+enabled_checkbutton.place(x=10, y=230)
+# можно сделать дохера галок с разными названиями/
+# Можно добавить разный текст на состояние галки
+# вот пример кода:
+# enabled_on = "Включено"
+# enabled_off = "Отключено"
+# enabled = StringVar(value=enabled_on)
+#
+# enabled_checkbutton = ttk.Checkbutton(textvariable=enabled, variable=enabled, offvalue=enabled_off, onvalue=enabled_on)
 
 win.update()  # обновляем информацию о виджетах
 
